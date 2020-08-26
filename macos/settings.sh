55de40706844e364d28557bcf85087e38e1041b7
ChrisCarini <6374067+ChrisCarini@users.noreply.github.com>
2020-08-25 19:04:59 -0700

    Adding headers to OSX settings.sh file.

--- macos/settings.sh
+++ macos/settings.sh
@@ -2,13 +2,21 @@
 
 # ~/.macos — https://mths.be/macos
 
-# Close any open System Preferences panes, to prevent them from overriding
-# settings we’re about to change
+function header() {
+  length=$((${#2} + 2))
+  s=$(printf "%-${length}s" "$1")
+  barz="${s// /$1}"
+  printf '\n%s\n> %s\n%s\n' "$barz" "$2" "$barz"
+}
+
+# ---------------------------------------------------
+header "-" "Close any open System Preferences panes." # Prevents them from overriding settings we’re about to change"
+# ---------------------------------------------------
 osascript -e 'tell application "System Preferences" to quit'
 
-###############################################################################
-# General UI/UX                                                               #
-###############################################################################
+# ========================
+header "=" "General UI/UX"
+# ========================
 
 # Reveal IP address, hostname, OS version, etc. when clicking the clock
 # in the login window
@@ -20,33 +28,48 @@ osascript -e 'tell application "System Preferences" to quit'
 #sudo rm -rf /System/Library/CoreServices/DefaultDesktop.jpg
 #sudo ln -s /path/to/your/image /System/Library/CoreServices/DefaultDesktop.jpg
 
-# Show the battery percentage
+# --------------------------------------
+header "-" "Show the battery percentage"
+# --------------------------------------
 defaults write com.apple.menuextra.battery ShowPercent YES
 
-# Show the Day of Week + Month + Date + 24hr time
+# ----------------------------------------------------------
+header "-" "Show the Day of Week + Month + Date + 24hr time"
+# ----------------------------------------------------------
 defaults write com.apple.menuextra.clock DateFormat -string "EEE MMM d  HH:mm:ss"
 
-# Change the Time Format to use 24-hour time (System Preferences > Language & Region > Time Format)
+# -----------------------------------------------------
+header "-" "Change the Time Format to use 24-hour time" # (System Preferences > Language & Region > Time Format)
+# -----------------------------------------------------
 defaults write -g AppleICUForce24HourTime -bool true
 
-# OSX Mojave - Turn off the ScreenShot preview in the lower right corner
+# ---------------------------------------------------------------------------------
+header "-" "OSX Mojave - Turn off the ScreenShot preview in the lower right corner"
+# ---------------------------------------------------------------------------------
 defaults write com.apple.screencapture show-thumbnail -bool false
 
-###############################################################################
-# Trackpad, mouse, keyboard, Bluetooth accessories, and input                 #
-###############################################################################
-
-# Disable “natural” (Lion-style) scrolling
+# ======================================================================
+header "=" "Trackpad, mouse, keyboard, Bluetooth accessories, and input"
+# ======================================================================
+# ---------------------------------------------------
+header "-" "Disable “natural” (Lion-style) scrolling"
+# ---------------------------------------------------
 defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
 
-# Disable press-and-hold for keys in favor of key repeat
+# -----------------------------------------------------------------
+header "-" "Disable press-and-hold for keys in favor of key repeat"
+# -----------------------------------------------------------------
 defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
 
-# Set a fast keyboard repeat rate
+# ------------------------------------------
+header "-" "Set a fast keyboard repeat rate"
+# ------------------------------------------
 defaults write NSGlobalDomain KeyRepeat -int 2
 defaults write NSGlobalDomain InitialKeyRepeat -int 25
 
-# Set language and text formats
+# ----------------------------------------
+header "-" "Set language and text formats"
+# ----------------------------------------
 # Note: if you’re in the US, replace `EUR` with `USD`, `Centimeters` with
 # `Inches`, `en_GB` with `en_US`, and `true` with `false`.
 defaults write NSGlobalDomain AppleLanguages -array "en-US"
@@ -54,42 +77,61 @@ defaults write NSGlobalDomain AppleLocale -string "en_US@currency=USD"
 defaults write NSGlobalDomain AppleMeasurementUnits -string "Inches"
 defaults write NSGlobalDomain AppleMetricUnits -bool false
 
-
-###############################################################################
-# Finder                                                                      #
-###############################################################################
-
-# Finder: show hidden files by default
+# =================
+header "=" "Finder"
+# =================
+# -----------------------------------------------
+header "-" "Finder: show hidden files by default"
+# -----------------------------------------------
 defaults write com.apple.finder AppleShowAllFiles -bool true
 
-# Finder: show all filename extensions
+# -----------------------------------------------
+header "-" "Finder: show all filename extensions"
+# -----------------------------------------------
 defaults write NSGlobalDomain AppleShowAllExtensions -bool true
 
-# Finder: show status bar
+# ----------------------------------
+header "-" "Finder: show status bar"
+# ----------------------------------
 defaults write com.apple.finder ShowStatusBar -bool true
 
-# Finder: show path bar
+# --------------------------------
+header "-" "Finder: show path bar"
+# --------------------------------
 defaults write com.apple.finder ShowPathbar -bool true
 
-# Keep folders on top when sorting by name
+# ---------------------------------------------------
+header "-" "Keep folders on top when sorting by name"
+# ---------------------------------------------------
 defaults write com.apple.finder _FXSortFoldersFirst -bool true
 
-# Change desktop icon side to be 60x60
-/usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:iconSize 60" ~/Library/Preferences/com.apple.finder.plist ; killall Finder
+# -----------------------------------------------
+header "-" "Change desktop icon side to be 60x60"
+# -----------------------------------------------
+/usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:iconSize 60" ~/Library/Preferences/com.apple.finder.plist
+killall Finder
 
-# Avoid creating .DS_Store files on network or USB volumes
+# -------------------------------------------------------------------
+header "-" "Avoid creating .DS_Store files on network or USB volumes"
+# -------------------------------------------------------------------
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
 defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
 
-# Automatically open a new Finder window when a volume is mounted
+# --------------------------------------------------------------------------
+header "-" "Automatically open a new Finder window when a volume is mounted"
+# --------------------------------------------------------------------------
 defaults write com.apple.frameworks.diskimages auto-open-ro-root -bool true
 defaults write com.apple.frameworks.diskimages auto-open-rw-root -bool true
 defaults write com.apple.finder OpenWindowForNewRemovableDisk -bool true
 
-# Finder: show Hard Drive on desktop
+# ---------------------------------------------
+header "-" "Finder: Show Hard Drive on desktop"
+# ---------------------------------------------
 defaults write com.apple.finder ShowHardDrivesOnDesktop -bool true
 
-# Finder: Add desired folders to Finder Favorites
+# ----------------------------------------------------------
+header "-" "Finder: Add desired folders to Finder Favorites"
+# ----------------------------------------------------------
 if ! [ -x "$(command -v mysides)" ]; then
   echo "mysides not installed - trying to install via 'brew cask install mysides'..."
   brew cask install mysides
@@ -113,135 +155,175 @@ if [ -x "$(command -v mysides)" ]; then
   mysides add "~DELETE THIS STUFF" file:///Users/$USER/Desktop/~DELETE%20THIS%20STUFF/
 fi
 
-
-###############################################################################
-# Dock, Dashboard, and hot corners                                            #
-###############################################################################
-
-# Enable highlight hover effect for the grid view of a stack (Dock)
+# ===========================================
+header "=" "Dock, Dashboard, and hot corners"
+# ===========================================
+# ----------------------------------------------------------------------------
+header "-" "Enable highlight hover effect for the grid view of a stack (Dock)"
+# ----------------------------------------------------------------------------
 defaults write com.apple.dock mouse-over-hilite-stack -bool true
 
-# Set the icon size of Dock items to 36 pixels
+# -------------------------------------------------------
+header "-" "Set the icon size of Dock items to 36 pixels"
+# -------------------------------------------------------
 defaults write com.apple.dock tilesize -int 36
 
-# Enable spring loading for all Dock items
+# ---------------------------------------------------
+header "-" "Enable spring loading for all Dock items"
+# ---------------------------------------------------
 defaults write com.apple.dock enable-spring-load-actions-on-all-items -bool true
 
-# Show indicator lights for open applications in the Dock
+# ------------------------------------------------------------------
+header "-" "Show indicator lights for open applications in the Dock"
+# ------------------------------------------------------------------
 defaults write com.apple.dock show-process-indicators -bool true
 
-# Don’t automatically rearrange Spaces based on most recent use
+# ------------------------------------------------------------------------
+header "-" "Don’t automatically rearrange Spaces based on most recent use"
+# ------------------------------------------------------------------------
 defaults write com.apple.dock mru-spaces -bool false
 
-
-###############################################################################
-# Safari & WebKit                                                             #
-###############################################################################
-
-# Privacy: don’t send search queries to Apple
+# ==========================
+header "=" "Safari & WebKit"
+# ==========================
+# ------------------------------------------------------
+header "-" "Privacy: don’t send search queries to Apple"
+# ------------------------------------------------------
 defaults write com.apple.Safari UniversalSearchEnabled -bool false
 defaults write com.apple.Safari SuppressSearchSuggestions -bool true
-
-# Show the full URL in the address bar (note: this still hides the scheme)
+# -----------------------------------------------------------------------------------
+header "-" "Show the full URL in the address bar (note: this still hides the scheme)"
+# -----------------------------------------------------------------------------------
 defaults write com.apple.Safari ShowFullURLInSmartSearchField -bool true
 
-# Enable the Develop menu and the Web Inspector in Safari
+# ------------------------------------------------------------------
+header "-" "Enable the Develop menu and the Web Inspector in Safari"
+# ------------------------------------------------------------------
 defaults write com.apple.Safari IncludeDevelopMenu -bool true
 defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
 defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled -bool true
 
-# Disable AutoFill
+# ---------------------------
+header "-" "Disable AutoFill"
+# ---------------------------
 defaults write com.apple.Safari AutoFillFromAddressBook -bool false
 defaults write com.apple.Safari AutoFillPasswords -bool false
 defaults write com.apple.Safari AutoFillCreditCardData -bool false
 defaults write com.apple.Safari AutoFillMiscellaneousForms -bool false
 
-# Warn about fraudulent websites
+# -----------------------------------------
+header "-" "Warn about fraudulent websites"
+# -----------------------------------------
 defaults write com.apple.Safari WarnAboutFraudulentWebsites -bool true
 
-# Disable plug-ins
+# ---------------------------
+header "-" "Disable plug-ins"
+# ---------------------------
 defaults write com.apple.Safari WebKitPluginsEnabled -bool false
 defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2PluginsEnabled -bool false
 
-# Disable Java
+# -----------------------
+header "-" "Disable Java"
+# -----------------------
 defaults write com.apple.Safari WebKitJavaEnabled -bool false
 defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2JavaEnabled -bool false
 
-# Block pop-up windows
+# -------------------------------
+header "-" "Block pop-up windows"
+# -------------------------------
 defaults write com.apple.Safari WebKitJavaScriptCanOpenWindowsAutomatically -bool false
 defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2JavaScriptCanOpenWindowsAutomatically -bool false
 
-# Enable “Do Not Track”
+# --------------------------------
+header "-" "Enable “Do Not Track”"
+# --------------------------------
 defaults write com.apple.Safari SendDoNotTrackHTTPHeader -bool true
 
-# Update extensions automatically
+# ------------------------------------------
+header "-" "Update extensions automatically"
+# ------------------------------------------
 defaults write com.apple.Safari InstallExtensionUpdatesAutomatically -bool true
 
-
-###############################################################################
-# Terminal & iTerm 2                                                          #
-###############################################################################
-
-# Only use UTF-8 in Terminal.app
+# =============================
+header "=" "Terminal & iTerm 2"
+# =============================
+# -----------------------------------------
+header "-" "Only use UTF-8 in Terminal.app"
+# -----------------------------------------
 defaults write com.apple.terminal StringEncodings -array 4
 
-# Set OSX terminal to automatically close upon exit
+# ------------------------------------------------------------
+header "-" "Set OSX terminal to automatically close upon exit"
+# ------------------------------------------------------------
 plutil -replace "Window Settings".Basic.shellExitAction -integer 1 ~/Library/Preferences/com.apple.Terminal.plist
 
-
-###############################################################################
-# Activity Monitor                                                            #
-###############################################################################
-
-# Show the main window when launching Activity Monitor
+# ===========================
+header "=" "Activity Monitor"
+# ===========================
+# ---------------------------------------------------------------
+header "-" "Show the main window when launching Activity Monitor"
+# ---------------------------------------------------------------
 defaults write com.apple.ActivityMonitor OpenMainWindow -bool true
 
-# Visualize CPU usage in the Activity Monitor Dock icon
+# ----------------------------------------------------------------
+header "-" "Visualize CPU usage in the Activity Monitor Dock icon"
+# ----------------------------------------------------------------
 defaults write com.apple.ActivityMonitor IconType -int 5
 
-# Show all processes in Activity Monitor
+# -------------------------------------------------
+header "-" "Show all processes in Activity Monitor"
+# -------------------------------------------------
 defaults write com.apple.ActivityMonitor ShowCategory -int 0
 
-# Sort Activity Monitor results by CPU usage
+# -----------------------------------------------------
+header "-" "Sort Activity Monitor results by CPU usage"
+# -----------------------------------------------------
 defaults write com.apple.ActivityMonitor SortColumn -string "CPUUsage"
 defaults write com.apple.ActivityMonitor SortDirection -int 0
 
-
-###############################################################################
-# TextEdit, and Disk Utility                                                  #
-###############################################################################
-
-# Use plain text mode for new TextEdit documents
+# =====================================
+header "=" "TextEdit, and Disk Utility"
+# =====================================
+# ---------------------------------------------------------
+header "-" "Use plain text mode for new TextEdit documents"
+# ---------------------------------------------------------
 defaults write com.apple.TextEdit RichText -int 0
-# Open and save files as UTF-8 in TextEdit
+
+# ---------------------------------------------------
+header "-" "Open and save files as UTF-8 in TextEdit"
+# ---------------------------------------------------
 defaults write com.apple.TextEdit PlainTextEncoding -int 4
 defaults write com.apple.TextEdit PlainTextEncodingForWrite -int 4
 
-# Enable the debug menu in Disk Utility
+# ------------------------------------------------
+header "-" "Enable the debug menu in Disk Utility"
+# ------------------------------------------------
 defaults write com.apple.DiskUtility DUDebugMenuEnabled -bool true
 defaults write com.apple.DiskUtility advanced-image-options -bool true
 
-
-###############################################################################
-# Misc OSX Customizations                                                     #
-###############################################################################
-# Silence the `zsh` suggestion in bash on OSX - ref: https://apple.stackexchange.com/a/371998
+# ==================================
+header "=" "Misc OSX Customizations"
+# ==================================
+# --------------------------------------------------------------------------------------------------------
+header "-" "Silence the \`zsh\` suggestion in bash on OSX - ref: https://apple.stackexchange.com/a/371998"
+# --------------------------------------------------------------------------------------------------------
 export BASH_SILENCE_DEPRECATION_WARNING=1
 
-###############################################################################
-# Kill affected applications                                                  #
-###############################################################################
+# =====================================
+header "=" "Kill affected applications"
+# =====================================
 
 # Don't kill the terminal, as that'd kill the script we're running from. ;)
-for app in "Activity Monitor" \
-	"cfprefsd" \
-	"Dock" \
-	"Finder" \
-	"Google Chrome Canary" \
-	"Google Chrome" \
-	"Safari" \
-	"SystemUIServer" \
-	; do
-	killall "${app}" &> /dev/null
+apps=(
+  "Activity Monitor"
+  "cfprefsd"
+  "Finder"
+  "Google Chrome Canary"
+  "Google Chrome"
+  "Safari"
+  "SystemUIServer"
+)
+for app in "${apps[@]}"; do
+  killall "${app}" &>/dev/null
 done
 echo "Done. Note that some of these changes require a logout/restart to take effect."
