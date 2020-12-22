d8913be3c4f4561b88ffd920d5a23008128664b4
ChrisCarini <6374067+ChrisCarini@users.noreply.github.com>
2020-12-21 22:56:43 -0800

    Make necessary changes for brew cask installations
    Fixes #26

--- install/brew-cask.sh
+++ install/brew-cask.sh
@@ -43,17 +43,17 @@ apps=(
 
 for APPLICATION in "${apps[@]}"; do
     # Check if the application is installed with `brew cask`
-    brew cask info $APPLICATION | grep "Not installed" > /dev/null 2>&1
+    brew info --cask $APPLICATION | grep "Not installed" > /dev/null 2>&1
     if [ $? -eq 0 ]; then
         # If the application is NOT installed via `brew cask`,
         # check if the application is installed in the system already
-        stat "/Applications/$(brew cask info $APPLICATION | grep -o ".*.app" | head -n 1)" > /dev/null 2>&1
+        stat "/Applications/$(brew info --cask $APPLICATION | grep -A1 Artifacts | tail -n1 | sed 's/\.app.*/.app/')" > /dev/null 2>&1
         if [ $? -eq 0 ]; then
             echo "$APPLICATION *is* installed, skipping..."
         else
             # Application is not installed, let's install it with `brew cask`
             # Note: The application *may* already be installed still; I'm unsure of a good way to identify this any further
-            brew cask install $APPLICATION
+            brew install --cask $APPLICATION
         fi
     else
         echo "$APPLICATION *is* installed, skipping..."
