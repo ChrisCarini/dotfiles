9719c3d0d3cbc69975d1abc3d2895f5f6b420b15
ChrisCarini <6374067+ChrisCarini@users.noreply.github.com>
2021-03-19 20:57:53 -0700

    Simplify brew-cask.sh logic.

--- install/brew-cask.sh
+++ install/brew-cask.sh
@@ -42,23 +42,10 @@ apps=(
 )
 
 for APPLICATION in "${apps[@]}"; do
-    # Check if the application is installed with `brew cask`
-    brew info --cask $APPLICATION | grep "Not installed" > /dev/null 2>&1
-    if [ $? -eq 0 ]; then
-        # If the application is NOT installed via `brew cask`,
-        # check if the application is installed in the system already
-        APP_INFO=$(brew info --cask $APPLICATION)
-        APP_INFO_RESULT=$?
-        stat "/Applications/$(brew info --cask $APPLICATION | grep -A1 Artifacts | tail -n1 | sed 's/\.app.*/.app/')" > /dev/null 2>&1
-        if [ $APP_INFO_RESULT -eq 0 && $? -eq 0 ]; then
-            echo "$APPLICATION *is* installed, skipping..."
-        else
-            # Application is not installed, let's install it with `brew cask`
-            # Note: The application *may* already be installed still; I'm unsure of a good way to identify this any further
-            # Call brew with the envvar to disable auto-update.
-            HOMEBREW_NO_AUTO_UPDATE=1 brew install --cask $APPLICATION
-        fi
-    else
-        echo "$APPLICATION *is* installed, skipping..."
-    fi
+  # NOTE: I used to check if the application was installed first.
+  #       That was too hard to maintain for the benefit (ie, none),
+  #       so I decided to remove the logic. Ahh, so simple. :)
+  echo "============================="
+  echo "Installing [$APPLICATION] ..."
+  HOMEBREW_NO_AUTO_UPDATE=1 brew install --cask $APPLICATION
 done
