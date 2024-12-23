0c4322b777ab4e34ac94f78b0bc11fa71b077fb2
ChrisCarini <6374067+ChrisCarini@users.noreply.github.com>
2024-12-23 07:44:05 -0800

    Fixing `captureWifiRSSI.sh` script to extract correct RSSI line, only show first value (bluetooth started showing); Adding (commented) line to remove erroneous '0' from the file (showed up because bluetooth devices).

--- macos/osx_wifi_strength/captureWifiRSSI.sh
+++ macos/osx_wifi_strength/captureWifiRSSI.sh
@@ -4,4 +4,6 @@
 #   ccarini         ALL = (ALL) NOPASSWD: /usr/bin/wdutil
 #
 # You MUST do ^ before running this script.
-echo "$(date +"%Y-%m-%d %H:%M:%S");$(sudo /usr/bin/wdutil info | grep RSSI | awk '{print $3}')" >> ~/wifi_rssi.csv
+echo "$(date +"%Y-%m-%d %H:%M:%S");$(sudo /usr/bin/wdutil info | \grep RSSI | awk '{print $3}' | head -n1)" >> ~/wifi_rssi.csv
+# Remove any lines with '0' RSSI values (these started showing up because Bluetooth devices were included, and my headphones showed '0' all the time)
+#sed -i '' '/^0$/d' ~/wifi_rssi.csv
